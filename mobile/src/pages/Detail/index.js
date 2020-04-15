f064047b917b20066c5e6906949df94e1ac8d3c3
Palmerio Julio <palmerio_julio@hotmail.com>
2020-04-15 00:01:59 -0300

    Colocando detalhes do caso.

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -48,6 +48,9 @@ export default function Detail() {
                 <Text style={styles.incidentProperty}>CASO:</Text>
                 <Text style={styles.incidentValue}>{incident.title}</Text>
 
+                <Text style={styles.incidentProperty}>Detalhe:</Text>
+                <Text style={styles.incidentValue}>{incident.description}</Text>
+
                 <Text style={styles.incidentProperty}>Valor:</Text>
                 <Text style={styles.incidentValue}>
                  {Intl.NumberFormat('pt-BR', { 
