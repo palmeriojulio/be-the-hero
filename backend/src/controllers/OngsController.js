4faa0c75a7b90853a25a4830291dab2ca2e9c8b9
Palmerio Julio <palmerio_julio@hotmail.com>
2020-04-14 22:36:01 -0300

    Testes

--- backend/src/controllers/OngsController.js
+++ backend/src/controllers/OngsController.js
@@ -1,4 +1,4 @@
-const crypto = require('crypto');
+const generateUniqueId = require('../utils/generateUniqueId');
 const connection = require('../database/connection');
 
 module.exports = {
@@ -11,9 +11,7 @@ module.exports = {
     async create(request, response) {
         const { name, email, whatsapp, city, uf } = request.body;
 
-        const id = crypto.randomBytes(4).toString('HEX');
-
-        console.log('const');
+        const id = generateUniqueId();
 
         await connection('ongs').insert({
             id,
--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -2,8 +2,10 @@
 const knex = require('knex');
 //importando o arquivo de configuração do banco.
 const configuration = require('../../knexfile');
+//setando variável ambiente para testes
+const config = process.env.NODE_ENV === 'test' ? configuration.test : configuration.development;
 //criando a conexão
-const connection = knex(configuration.development);
+const connection = knex(config);
 
 //exportando a conexão
 module.exports = connection;
